0e7b127775a8a320545da7a5805672e3e6ba5f4a
Jose Luis Jiménez <joseluisjb@ufps.edu.co>
2026-05-20 17:55:40 -0500

    RF43 agregando descripción y categoria de Producto para landingPage

--- src/main/java/com/gelox/backend/repositories/ProductoRepository.java
+++ src/main/java/com/gelox/backend/repositories/ProductoRepository.java
@@ -1,5 +1,6 @@
 package com.gelox.backend.repositories;
 
+import com.gelox.backend.entities.CategoriaProducto;
 import com.gelox.backend.entities.Producto;
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -9,4 +10,6 @@ import java.util.UUID;
 public interface ProductoRepository extends JpaRepository<Producto, UUID> {
 
     List<Producto> findByActivoTrue();
+
+    List<Producto> findByActivoTrueAndCategoria(CategoriaProducto categoria);
 }
